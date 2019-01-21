9db37df0a7df6b2f5a4528a05435e4d8245cb691
Greg Smith <gregsmith446@gmail.com>
2019-01-21 18:51:33 -0500

    partially working version

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintingCharsAtIndicesInStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number of strings to expect");
+            int T = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("type that number of strings");
+            string input = Console.ReadLine();
+
+            StringBuilder outputOdd = new StringBuilder();
+            StringBuilder outputEven = new StringBuilder();
+
+            if (numOfArgs == 1)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        outputEven.Append(input[i]);
+                    }
+                }
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        outputOdd.Append(input[i]);
+                    }
+                }
+
+                Console.Write(outputEven + " ");
+                Console.WriteLine(outputOdd);
+            }
+
+           
+
+        }
+    }
+}
